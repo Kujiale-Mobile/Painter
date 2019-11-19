4ec44ed7d39dd2f52ffa2b2b3535decf0c4047b9
0JARVIS0 <709406687@qq.com>
2019-11-19 09:59:26 +0800

    增加恢复功能，完善添加与删除功能

--- pages/example/example.js
+++ pages/example/example.js
@@ -1,9 +1,17 @@
 import Card from '../../palette/card';
 
+const ACTION_NAMES = {
+  MOVE: 'move',
+  RESIZE: 'resize',
+  ADD: 'add',
+  DELETE: 'delete',
+  CHANGE: 'change'
+}
 // src/pages/xml2can/xml2can.js
 Page({
   imagePath: '',
   history: [],
+  future: [],
 
   /**
    * 页面的初始数据
@@ -24,16 +32,65 @@ Page({
     if (!pre) {
       return
     }
-    for (let view of this.data.template.views) {
-      if (view.id === pre.id) {
-        view.css = pre.css
-        break
+    const needRefresh = pre.index >= 0 && pre.index <= this.data.template.views.length
+    if (needRefresh) {
+      if (this.data.template.views[pre.index].id === pre.view.id) {
+        this.data.template.views.splice(pre.index, 1)
+      } else {
+        this.data.template.views.splice(pre.index, 0, pre.view)
+      }
+      this.future.push(pre)
+    } else {
+      for (let i in this.data.template.views) {
+        if (this.data.template.views[i].id === pre.view.id) {
+          this.future.push({ view: this.data.template.views[i] })
+          this.data.template.views[i] = pre.view
+          break
+        }
       }
     }
-    this.setData({
-      action: pre,
+    const props = {
       paintPallette: this.data.template,
-    })
+    }
+    if (needRefresh) {
+      props.template = this.data.template
+    } else {
+      props.action = pre
+    }
+    this.setData(props)
+  },
+
+  onRecover() {
+    const fut = this.future.pop()
+    if (!fut) {
+      return
+    }
+    const needRefresh = fut.index >= 0 && fut.index <= this.data.template.views.length
+    if (needRefresh) {
+      if (this.data.template.views[fut.index].id === fut.view.id) {
+        this.data.template.views.splice(fut.index, 1)
+      } else {
+        this.data.template.views.splice(fut.index, 0, fut.view)
+      }
+      this.history.push(fut)
+    } else {
+      for (let i in this.data.template.views) {
+        if (this.data.template.views[i].id === fut.view.id) {
+          this.history.push({ view: this.data.template.views[i] })
+          this.data.template.views[i] = fut.view
+          break
+        }
+      }
+    }
+    const props = {
+      paintPallette: this.data.template,
+    }
+    if (needRefresh) {
+      props.template = this.data.template
+    } else {
+      props.action = fut
+    }
+    this.setData(props)
   },
 
   saveImage() {
@@ -42,28 +99,45 @@ Page({
     });
   },
 
-  touchEnd(e) {
-    for (let view of this.data.template.views) {
-      if (view.id === e.detail.id) {
-        this.history.push({
-          id: view.id,
-          css: view.css
-        })
-        view.css = e.detail.css
-        break
+  touchEnd({ detail }) {
+    let needRefresh = detail.index >= 0 && detail.index <= this.data.template.views.length
+    if (needRefresh) {
+      this.history.push({
+        ...detail
+      })
+      if (this.data.template.views[detail.index].id === detail.view.id) {
+        this.data.template.views.splice(detail.index, 1)
+      } else {
+        this.data.template.views.splice(detail.index, 0, detail.view)
+      }
+    } else {
+      for (let view of this.data.template.views) {
+        if (view.id === detail.view.id) {
+          this.history.push({
+            view: {
+              ...detail.view,
+              ...view
+            }
+          })
+          view.css = detail.view.css
+          break
+        }
       }
     }
-    console.log(this.history)
-
-    this.setData({
+    this.future.length = 0
+    const props = {
       paintPallette: this.data.template,
-    });
+    }
+    if (needRefresh) {
+      props.template = this.data.template
+    }
+    this.setData(props);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     this.setData({
       template: new Card().palette(),
     });
